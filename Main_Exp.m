b17fa7e77734a5d4cb646bb80fdc9bc9c7d9990a
bianlab <57670356+bianlab@users.noreply.github.com>
2024-07-30 23:09:43 +0200

    Add files via upload

--- Main_Exp.m
+++ Main_Exp.m
@@ -10,9 +10,10 @@ pixelSize = 1.67e-6 / Compressive_ratio;     % Camera pixel size
 z_d1 = 0.708e-3;             % Distance between plaintext to scattering layer
 z_d2 = 0.682e-3;             % Distance between scattering layer to camera
 load('./ExpData/USAF.mat'); % scattering layer shift position
-load('./ExpData/CalibratedPattern/Scatterer_pattern.mat'); % scattering layer shift position
+load('./ExpData/CalibratedPattern/Scatterer_pattern_real.mat'); % scattering layer shift position
+load('./ExpData/CalibratedPattern/Scatterer_pattern_imag.mat'); % scattering layer shift position
 load('./ScattererShift/loc_dftpc_XY_16_Exp.mat')
-
+PatternRecovery = Pattern_real + 1j.*Pattern_imag;
 %%  [3] Hybrid-driven decryption
 Rec = HybridDecryption_Exp(y, PatternRecovery, Compressive_ratio, pixelSize, waveLength,z_d1, z_d2, x_shift, y_shift,0.03,0.35);
 
